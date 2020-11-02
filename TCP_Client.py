b42d5e7b4623164a23cea32b7399cda78366ebf1
aytankhalilova <64959696+aytankhalilova@users.noreply.github.com>
2020-11-02 20:46:58 +0400

    Add files via upload

---
+++ TCP_Client.py
@@ -0,0 +1,55 @@
+import socket
+import json
+import argparse
+import os
+import sys
+import TCP_Server
+
+INTERFACE = '127.0.0.1'
+PORT = 1060
+BUFFSIZE = 2048
+SEPARATOR = "*"
+
+class TcpClient:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def connect(self, mode, source_file, key_or_json_file):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        print('Socket name of client:', sock.getsockname())
+        size_of_srcfile = os.path.getsize(source_file)
+        size_of_kjfile = os.path.getsize(key_or_json_file)
+        print('Sending mode, source file and {} file size to TCP_Server:'.format("json" if mode=="change_text" else "key"))
+        sock.send(f"{mode}{SEPARATOR}{size_of_srcfile}{SEPARATOR}{size_of_kjfile}".encode())
+        with open(source_file, "r") as f:
+            data_of_srcfile = f.read()
+            print(data_of_srcfile)
+            print("Sending source file as bytes to server:")
+            sock.sendall(bytes(data_of_srcfile.encode()))
+            print((data_of_srcfile.encode()))
+            f.close()
+        with open(key_or_json_file, "r") as f:
+            data_of_kjfile = f.read() if mode == "encode_decode" else json.dumps(f)
+            print(f'Sending {"json" if mode=="change_text" else "key"} file as  bytes to server:')
+            sock.sendall(bytes(data_of_kjfile.encode()))
+            f.close()
+
+        print('Receiving conclusion from TCP_Server:')
+        received = sock.recv(size_of_srcfile).decode()
+        print('Server replied with:', str(received))
+        sock.close()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', choices={'change_text', 'encode_decode'})
+    parser.add_argument('srcfile')
+    parser.add_argument('kjfile')
+    args = parser.parse_args()
+    print("-------------------")
+    TcpClient(INTERFACE, PORT).connect(args.mode, args.srcfile, args.kjfile)
+
+
+if __name__ == '__main__':
+    main()
---
+++ TCP_Server.py
@@ -0,0 +1,66 @@
+import socket
+import json
+
+INTERFACE = '127.0.0.1'
+PORT = 1060
+BUFFSIZE = 2048
+SEPARATOR = "*"
+
+
+class TcpServer:
+    def __init__(self, interface, port):
+        self.interface = interface
+        self.port = port
+
+    def bind(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((self.interface, self.port))
+        sock.listen(1)
+        print('Listening at', sock.getsockname())
+
+        while True:
+            sc, sockname = sock.accept()
+            print('We have accepted connection from ', sockname)
+            print('Receiving mode, file size and json/key file size:')
+            received = sc.recv(BUFFSIZE).decode()
+            mode, size_of_file, size_of_key_or_json = received.split(SEPARATOR)
+            size_of_file = int(size_of_file)
+            size_of_key_or_json = int(size_of_key_or_json)
+
+            print('Received source file data :')
+            data_of_srcfile = sc.recv(size_of_file).decode()
+
+            print("Received {} data: ".format('json' if mode == 'change_text' else 'key'))
+            data_of_kjfile = sc.recv(size_of_key_or_json).decode()
+            if mode=="change_text":
+                data_of_kjfile = json.loads(data_of_kjfile)
+
+            conclusion = ""
+            if str(mode) == "change_text":
+                conclusion = self.change_text(data_of_srcfile, data_of_kjfile)
+            else:
+                conclusion = self.encode_decode(data_of_srcfile, data_of_kjfile)
+                print(conclusion)
+
+
+            print('Sending conclusion to the client:')
+            sc.sendall(bytes(conclusion.encode()))
+            sc.close()
+            print("Session closed.")
+
+    def change_text(self, source_file, json_file):
+        for i in json_file:
+            source_file = source_file.replace(i, json_file[i])
+        return source_file
+
+
+    def encode_decode(self, source_file, key_file):
+        return "".join(chr(ord(source_file[j]) ^ ord(key_file[j%len(key_file)])) for (j) in range(len(source_file)))
+
+def main():
+    TcpServer(INTERFACE, PORT).bind()
+
+if __name__ == '__main__':
+    main()
+
